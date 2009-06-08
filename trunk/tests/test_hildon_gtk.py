9b41b34721978563f4d22adbf31be75640e99990
lmoura <lmoura@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-06-08 22:26:16 +0000

    Adding hildon.GtkRadioButton, hildon.WindowStack.get_default
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1055 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- trunk/tests/test_hildon_gtk.py
+++ trunk/tests/test_hildon_gtk.py
@@ -9,6 +9,21 @@ class TestHildonGtkButton(unittest.TestCase):
         t = hildon.GtkButton(gtk.HILDON_SIZE_AUTO)
         self.assertTrue(type(t) is hildon.GtkButton)
         self.assertRaises(TypeError, hildon.GtkButton)
+       
+class TestHildonGtkButtonConstructor(unittest.TestCase):
+
+    def setUp(self):
+        self.size_policy = gtk.HILDON_SIZE_THUMB_HEIGHT
+    
+    def testBasic(self):
+        a = hildon.GtkRadioButton(self.size_policy)
+        self.assertTrue(type(a) is hildon.GtkRadioButton)
+        self.assertRaises(TypeError, hildon.GtkRadioButton)
+
+    def testGroupConstructor(self):
+        first = hildon.GtkRadioButton(self.size_policy)
+        second = hildon.GtkRadioButton(self.size_policy, first)
+        
 
 class TestHildonGtkTreeView(unittest.TestCase):
     def test_constructor(self):
