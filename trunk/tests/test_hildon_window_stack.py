8faa1ecbdcbc4eb9e07f85c16e263979354a6630
lizardo <lizardo@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-07-15 14:10:23 +0000

    python-hildon: implement hildon.WindowStack.push_list and hildon.WindowStack.get_windows
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1214 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- trunk/tests/test_hildon_window_stack.py
+++ trunk/tests/test_hildon_window_stack.py
@@ -3,18 +3,23 @@ import unittest
 import gtk
 import hildon
 
-class TestSetDefault(unittest.TestCase):
-    def setUp(self):
-        self.window = hildon.StackableWindow()
-
-    def tearDown(self):
-        del self.window
-
-    def testGetDefault(self):
-        # TODO improve this check
+class TestHildonWindowStack(unittest.TestCase):
+    def test_get_default(self):
         default_stack = hildon.WindowStack.get_default()
         self.assert_(isinstance(default_stack, hildon.WindowStack))
 
+    def test_get_windows(self):
+        stack = hildon.WindowStack()
+        w = hildon.StackableWindow()
+        stack.push_1(w)
+        self.assertTrue(stack.get_windows(), [w])
+
+    def test_push_list(self):
+        stack = hildon.WindowStack()
+        w1 = hildon.StackableWindow()
+        w2 = hildon.StackableWindow()
+        stack.push_list([w1, w2])
+        self.assertTrue(stack.get_windows(), [w1, w2])
+
 if __name__ == '__main__':
     unittest.main()
-
