9ff401a50b9e81817f005f783cb6ca2e3302abb3
lizardo <lizardo@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2009-11-11 20:30:30 +0000

    python-hildon: hildon.Note: allow note_type argument to also receive a hildon.NoteType (Fixes MB#5243).
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1438 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- trunk/tests/test_hildon_note.py
+++ trunk/tests/test_hildon_note.py
@@ -14,7 +14,17 @@ class TestHildonNote(unittest.TestCase):
         self.assertTrue(isinstance(note, hildon.Note))
         note = hildon.Note("cancel", w, "xyz", progressbar=gtk.ProgressBar())
         self.assertTrue(isinstance(note, hildon.Note))
-        self.assertRaises(TypeError, hildon.Note, "cancel", w, "xyz")
+
+        note = hildon.Note(hildon.NOTE_TYPE_PROGRESSBAR, w, "xyz", progressbar=gtk.ProgressBar())
+        self.assertTrue(isinstance(note, hildon.Note))
+        note = hildon.Note(hildon.NOTE_TYPE_CONFIRMATION, w, "xyz")
+        self.assertTrue(isinstance(note, hildon.Note))
+        note = hildon.Note(hildon.NOTE_TYPE_INFORMATION, w, "xyz")
+        self.assertTrue(isinstance(note, hildon.Note))
+
+        self.assertRaises(ValueError, hildon.Note, "invalid_type", w, "xyz")
+        self.assertRaises(ValueError, hildon.Note, ("invalid_type"), w, "xyz")
+        self.assertRaises(TypeError, hildon.Note, "cancel")
 
 if __name__ == "__main__":
     unittest.main()
