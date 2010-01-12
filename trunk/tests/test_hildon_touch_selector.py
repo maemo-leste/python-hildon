6575b81e6f2b8ca1b66c8df3a0e3cf07f9d839af
lizardo <lizardo@f8ba266b-2117-0410-b6ba-c3dd9a426789>
2010-01-12 19:45:13 +0000

    python-hildon: implement get_selected_rows() method (Fixes: MB#7818).
    git-svn-id: https://vcs.maemo.org/svn/pymaemo@1477 f8ba266b-2117-0410-b6ba-c3dd9a426789

--- trunk/tests/test_hildon_touch_selector.py
+++ trunk/tests/test_hildon_touch_selector.py
@@ -1,23 +1,35 @@
 import unittest
-from sys import getrefcount
 
 import hildon
 import gtk
 import gobject
 
+test_list = ["one", "two", "three"]
+
 class TestHildonTouchSelector(unittest.TestCase):
     def test_append_column(self):
         ts = hildon.TouchSelector()
-	
-	store = gtk.ListStore(gobject.TYPE_STRING)
-	i = store.append(["GTK_OK"])
-	store.set(i, 0, "GTK_OK")
-	
-	cell = gtk.CellRendererPixbuf()
-	col = ts.append_column(store, cell, stock_id=0)
-	self.assertTrue(isinstance(col, hildon.TouchSelectorColumn))
+        store = gtk.ListStore(gobject.TYPE_STRING)
+        for i in test_list:
+            store.append([i])
+        cell = gtk.CellRendererPixbuf()
+        col = ts.append_column(store, cell, stock_id=0)
+        self.assertTrue(isinstance(col, hildon.TouchSelectorColumn))
+        self.assertEqual([i[0] for i in ts.get_model(0)], test_list)
 
-	
+    def test_get_selected_rows(self):
+        ts = hildon.TouchSelector()
+        store = gtk.ListStore(gobject.TYPE_STRING)
+        for i in test_list:
+            store.append([i])
+        ts.append_column(store, gtk.CellRendererPixbuf())
+        # First item is selected by default
+        self.assertEqual(ts.get_selected_rows(0), [(0,)])
+        # Select second item and see if get_selected_rows() reflects the change
+        first = ts.get_model(0).get_iter_first()
+        second = ts.get_model(0).iter_next(first)
+        ts.select_iter(0, second, False)
+        self.assertEqual(ts.get_selected_rows(0), [(1,)])
 
 if __name__ == "__main__":
     unittest.main()
