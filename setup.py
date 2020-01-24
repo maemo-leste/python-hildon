38722214ed6cfb451c6ff8df3cb2fab2d6e03a4b
Merlijn Wajer <merlijn@wizzup.org>
2020-01-24 16:13:04 +0100

    Remove gvfs

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ hildon = TemplateExtension(name = "hildon",
                            pygobject_pkc = ("hildon-fm-2",
                                             "pygobject-2.0",
                                             "gconf-2.0", # needed by hildon-fm-2 (see MB#4538)
-                                            "gnome-vfs-2.0", # needed by hildon-fm-2 (see MB#4538)
                                             "hildon-notify",
                                             "dbus-1",
                                             "dbus-glib-1",
